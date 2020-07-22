83a101aaa35863649e862c972da2d6cb82da7cc2
Alex <format37@gmail.com>
2020-07-22 14:03:16 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -9,12 +9,13 @@ import urllib
 import ssl
 
 SCRIPT_PATH	= '/home/dvasilev/projects/ws_monitoring/'
+SCRIPT_PATH	= '/home/dvasilev/cert/'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 #WEBHOOK_PORT = 8082
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = SCRIPT_PATH+'webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = SCRIPT_PATH+'webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = CERT_PATH+'fullchain.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = CERT_PATH+'privkey.pem'  # Path to the ssl private key
 
 async def call_check(request):
 	return web.Response(text='ok',content_type="text/html")
