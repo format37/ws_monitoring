9b7f320c4929cf4e9d64ab9cddfbea76d09117ad
Alex <format37@gmail.com>
2020-07-24 15:17:46 +0300

    modified:   ruvds_translator.py

--- ruvds_translator.py
+++ ruvds_translator.py
@@ -1,1 +1,18 @@
-x
+PORT = 8082
+from aiohttp import web
+import asyncio
+
+async def call_check(request):
+	return web.Response(text='ok',content_type="text/html")
+
+rover_init()
+
+app = web.Application()
+app.router.add_route('GET', '/check',	call_check)
+
+# Start aiohttp server
+web.run_app(
+    app,
+    host='127.0.0.1',
+    port=PORT,
+)
